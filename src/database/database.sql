7b8e9e87fd71452970f7683a96029f08f869782b
Yuliia Maiboroda <150142827+yuliia-maiboroda@users.noreply.github.com>
2023-11-09 22:17:17 +0200

    feature: created categories and transactions entities

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,19 +1,4 @@
--- Define custom types
--- CREATE TYPE category AS (
---     id SERIAL PRIMARY KEY,
---     label VARCHAR(50) NOT NULL,
---     type VARCHAR(10) CHECK (type IN ('income', 'expense')) NOT NULL,
---     owner INTEGER,
---     FOREIGN KEY(owner) REFERENCES users(id)
--- );
 
--- CREATE TYPE transaction AS (
---     id SERIAL PRIMARY KEY,
---     label VARCHAR(50) NOT NULL,
---     amount NUMERIC 
---     owner INTEGER,
---     FOREIGN KEY(owner) REFERENCES users(id)
--- );
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS users (
@@ -21,14 +6,30 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) UNIQUE NOT NULL,
     displayname VARCHAR(50) NOT NULL,
     role VARCHAR(10) CHECK (role IN ('user', 'admin')) NOT NULL,
-    password VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     sessionId UUID
 );
+
+
+CREATE TABLE IF NOT EXISTS default_categories (
+    id SERIAL PRIMARY KEY,
+    label VARCHAR(50) NOT NULL,
+    type VARCHAR(10) CHECK (type IN ('income', 'expense')) NOT NULL,
+    isMutable BOOLEAN NOT NULL DEFAULT true
+);
+
+INSERT INTO default_categories (label, type, isMutable)
+VALUES ('Salary', 'income', true),
+       ('Food', 'expense', true),
+       ('Groceries', 'expense', true),
+       ('Entertainment', 'expense', true),
+       ('Others', 'expense', false);
 
 CREATE TABLE IF NOT EXISTS categories (
     id SERIAL PRIMARY KEY,
     label VARCHAR(50) NOT NULL,
     type VARCHAR(10) CHECK (type IN ('income', 'expense')) NOT NULL,
+    isMutable BOOLEAN NOT NULL DEFAULT true,
     owner INTEGER,
     FOREIGN KEY(owner) REFERENCES users(id)
 );
@@ -38,11 +39,8 @@ CREATE TABLE IF NOT EXISTS transactions (
     label VARCHAR(50) NOT NULL,
     amount NUMERIC,
     owner INTEGER,
-    FOREIGN KEY(owner) REFERENCES users(id)
+    FOREIGN KEY(owner) REFERENCES users(id),
+    category INTEGER,
+    FOREIGN KEY(category) REFERENCES categories(id)
 );
 
-CREATE TABLE IF NOT EXISTS default_categories (
-    id SERIAL PRIMARY KEY,
-    label VARCHAR(50) NOT NULL,
-    type VARCHAR(10) CHECK (type IN ('income', 'expense')) NOT NULL
-);
